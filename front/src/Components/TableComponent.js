a03572fa57e950248c60500b61fdbb2e5a6a32b2
Tanaka Takehiro <take.tennis.8227@gmail.com>
2019-03-30 17:29:33 +0900

    Read.me記述

---
+++ front/src/Components/TableComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+class Table extends React.Component {
+  render(){
+    return(
+      <AllArea>
+        Hello world
+      </AllArea>
+    )
+  }
+}
+
+export default Table;
+
+const AllArea = styled.div`
+  text-align: center;
+`
